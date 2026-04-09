4baf728e22b0daa1d874b20bcc0bbe9815015900
Radika Ist <11767310+radikaist@users.noreply.github.com>
2026-04-09 15:51:44 +0700

    commit

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -1,40 +1,40 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
-    <a href="<?= BASE_URL; ?>" class="brand-link">
-        <span class="brand-text font-weight-light font-weight-bold ml-3">RON PHP Framework</span>
-    </a>
+        <a href="<?= BASE_URL; ?>" class="brand-link">
+            <span class="brand-text font-weight-light font-weight-bold ml-3">RON PHP Framework</span>
+        </a>
 
-    <div class="sidebar">
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="https://ui-avatars.com/api/?name=Admin+IT&background=0D8ABC&color=fff" class="img-circle elevation-2" alt="User Image">
+        <div class="sidebar">
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="https://ui-avatars.com/api/?name=Admin+IT&background=0D8ABC&color=fff" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block">Chief IT Division</a>
+                </div>
             </div>
-            <div class="info">
-                <a href="#" class="d-block">Chief IT Division</a>
-            </div>
-        </div>
 
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
-                    <a href="<?= BASE_URL; ?>" class="nav-link active">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li class="nav-header">MANUFAKTUR</li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-industry"></i>
-                        <p>Production</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-boxes"></i>
-                        <p>Inventory</p>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        </div>
-    </aside>
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item">
+                        <a href="<?= BASE_URL; ?>" class="nav-link active">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li class="nav-header">MANUFAKTUR</li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-industry"></i>
+                            <p>Production</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-boxes"></i>
+                            <p>Inventory</p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            </div>
+        </aside>

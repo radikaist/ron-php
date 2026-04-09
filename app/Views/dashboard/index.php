f4a7237137e58981a481aa5eab8ce000877aca1f
Radika Ist <11767310+radikaist@users.noreply.github.com>
2026-04-09 15:50:08 +0700

    commit

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -22,24 +22,3 @@
             </div>
         </section>
         </div>
-    ```
-
-### 3. File `app/Views/layouts/footer.php`
-Pastikan isinya murni hanya kode ini:
-
-```html
-    <footer class="main-footer">
-        <strong>&copy; 2026 PT. Matsuo Precision Indonesia.</strong>
-        All rights reserved.
-        <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 1.0.0-dev
-        </div>
-    </footer>
-
-</div>
-<script src="[https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js](https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js)"></script>
-<script src="[https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js](https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js)"></script>
-<script src="[https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js](https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js)"></script>
-
-</body>
-</html>

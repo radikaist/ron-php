4610415650775b94e0405a8c95570692904c2319
Radika Ist <11767310+radikaist@users.noreply.github.com>
2026-04-09 15:00:42 +0700

    commit

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
-// Mulai session untuk kebutuhan login/RBAC nanti
+// TAMPILKAN ERROR: Baris ini sangat penting selama masa development
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Mulai session untuk kebutuhan login/RBAC
 if (!session_id()) session_start();
 
 // Panggil file inti framework
+// CATATAN PENTING: Pastikan huruf besar/kecil di struktur folder Anda 
+// sama persis dengan '../app/Core/App.php'
 require_once '../app/Core/App.php';
 require_once '../app/Core/Controller.php';
 

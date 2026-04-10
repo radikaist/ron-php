474a37585c1c6fa1fd499b1c25789368c59d3905
Radika Ist <11767310+radikaist@users.noreply.github.com>
2026-04-10 08:44:37 +0700

    commit

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-        <strong>&copy; 2026 PT. Matsuo Precision Indonesia.</strong>
+        <strong>&copy; 2026 Radika Origin Native PHP (RON PHP).</strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
             <b>Version</b> 1.0.0-dev
